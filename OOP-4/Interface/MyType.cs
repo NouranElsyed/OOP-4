6ce07b3e5f54094ea3357f773c5a72edb7b5b17a
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-21 17:39:20 +0300

    Part 01 Interface

---
+++ OOP-4/Interface/MyType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_4.Interface
+{
+    internal class MyType : IMyType
+    {
+        public int Id { get; set; }
+
+        public void MyFun(int x)
+        {
+            Console.WriteLine($"Hello Route : X = {x} , Id = {Id}");
+        }
+    }
+}
--- OOP-4/Program.cs
+++ OOP-4/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace OOP_4
+﻿using OOP_4.Interface;
+
+namespace OOP_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            MyType myType = new MyType();
+            myType.Id = 30;
+            myType.MyFun(100);
+            //myType.Print(); // invalid
+
+            IMyType myType1 = new MyType();
+            myType1.Id = 60;
+            myType1.MyFun(600);
+            myType1.Print(); // valid
 
         }
     }
