c4709b891fd022e8330d876abf05950b1573a2bb
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-22 15:52:33 +0300

    part20Q3

---
+++ assignment4/part02Q2/BasicAuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4.part02Q2
+{
+    internal class BasicAuthenticationService : IAuthenticationService
+    {
+        public bool AuthenticateUser(string username, int password)
+        {
+            if (username=="Nouran" && password==123) { return true; }
+            return false;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            if (username == "Nouran" && role == "Admin") { return true; }
+            return false;
+        }
+    }
+}
---
+++ assignment4/part02Q3/PushNotificationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4.part02Q3
+{
+    internal class PushNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string message)
+        {
+            Console.WriteLine($" notification sent to {recipient}");
+            Console.WriteLine($"message {message}");
+        }
+    }
+}
